e7cb384964ba31b7ed602ec57da4b0ef96f6166c
meepowlz <meepowlz@gmail.com>
2021-12-09 14:30:47 -0500

    Added home route

--- server.py
+++ server.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+port = 8080
+
+@app.route("/")
+def home_route():
+    return "Home page"
+
+app.run(host="localhost", port=port)
