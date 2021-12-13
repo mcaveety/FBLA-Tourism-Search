deb0400c7d557b7d4d2005da4c942a66ce31668e
meepowlz <meepowlz@gmail.com>
2021-12-13 14:38:51 -0500

    Added get/post methods

--- server.py
+++ server.py
@@ -3,13 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 port = 8080
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home_route():
-    return "Home page"
+    if request.method == "GET":
+        return render_template("table.html", testing="CSV File Data")
 
-@app.route("/test")
-def test_route():
-    return render_template("table.html", testing="This is the table text")
+# @app.route("/test")
+# def test_route():
+#     return render_template("table.html", testing="This is the table text")
 
 app.run(host="localhost", port=port)
 
