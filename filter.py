c560b1b8abc9057a4fbe92eb1f62a712641e89a6
meepowlz <meepowlz@gmail.com>
2021-12-24 20:23:58 -0500

    Updated for json format

--- filter.py
+++ filter.py
@@ -1,12 +1,8 @@
-# 12.10.21
 # Filters data from locations.csv based on user input
+import json
 
-import csv
-
-with open('locations.csv') as locations:
-    reader = csv.reader(locations)
-    for row in reader:
-        print(row)
+with open("locations.json") as j:
+    data = json.load(j)
 
 # Displays full list of attractions
 def resetFilter():
