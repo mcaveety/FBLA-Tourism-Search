ac95ca58c9313ac7fb1b6eb161597f18e336e325
meepowlz <meepowlz@gmail.com>
2021-12-27 16:14:32 -0500

    imported filter.py and started doin things

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import filter
 
 app = Flask(__name__)
 port = 8080
@@ -7,13 +8,14 @@ port = 8080
 def home_route():
     if request.method == "GET":
         return render_template("table.html", testing="CSV File Data")
-    # Should this be an IF statement?
     if request.method == "POST":
         print(request.form.get("name"))
         print(request.form.get("attractionType"))
-        print(request.form.get("townName"))
+        print(request.form.get("price"))
         print(request.form.get("groupSize"))
         print(request.form.get("locationRating"))
+        filter.filterType("name", request.form.get("name"))
+        filter.filterType("type", request.form.get("attractionType"))
         return "Search successful"
 
 app.run(host="localhost", port=port)
