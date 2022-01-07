84cf1a655da09efda6b024a324a6387b3be0bc54
meepowlz <meepowlz@gmail.com>
2022-01-07 18:36:56 -0500

    Added comments

--- filter.py
+++ filter.py
@@ -1,13 +1,18 @@
 # Filters data from locations.json based on user input
-# Returns a list
+# Yields a generator containing matching locations
+
 import json
-from flask import url_for
 
 # Opens data file for filtering
 with open("static/locations.json") as j:
     data = json.load(j)
 
-# Searches through data based on user inputs gathered from POST request
+"""
+Searches through data based on user inputs gathered from POST request
+If multiple types are selected, the filter is run multiple times
+Each time, the data is filtered by one of the selected types
+The filtered generator is then iterated through again and filtered further
+"""
 def locationsFilter(type, input, list):
     for location in list:
         if input.lower() in location[type].lower():
