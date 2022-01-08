6720ffce77725819426b12758506eaf29b741d58
meepowlz <meepowlz@gmail.com>
2022-01-08 18:59:59 -0500

    Fix folder loading for pyinstaller

--- filter.py
+++ filter.py
@@ -1,15 +1,28 @@
 # Filters data from locations.json based on user input
 # Yields a generator containing matching locations
 
-import json
+import json, os, sys
+
+# Allows data to be accessed after PyInstaller bundles the program up
+# Code credit: https://stackoverflow.com/a/13790741
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 
 # Opens data file for filtering
-with open("json/locations.json") as j:
+with open(resource_path("json/locations.json")) as j:
     data = json.load(j)
 
 # Backs up data to a second file
 def backup():
-    with open("json/locations2.json", "w") as f:
+    with open(resource_path("json/locations2.json"), "w") as f:
         json.dump(data, f, indent=4)
 
 
